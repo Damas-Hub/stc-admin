566f84c8b809ed29e426bfac29da1fcb8d663dd0
Damas-Hub <selormeyh@gmail.com>
2025-06-06 11:16:18 +0000

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,27 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import { ThemeProvider } from 'next-themes'
+import { Inter } from 'next/font/google'
 
 export const metadata: Metadata = {
-  title: 'STC Ghana Dashboard',
+  title: 'STC Ghana Dashboard', 
   description: 'STC Ghana Dashboard',
   generator: 'STC Ghana Dashboard',
 }
 
+const inter = Inter({ subsets: ['latin'] })
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} antialiased`} suppressHydrationWarning>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
